4275d1f15e3b7589b99442576ed7f6ec4d37ae8a
Nay Htet <nayhtet.saw@gmail.com>
2022-03-17 17:22:46 +0630

    Update countOdd.php

--- countOdd.php
+++ countOdd.php
@@ -1,13 +1,13 @@
 <?php
 
 function countOdds($low, $high) {
-    $count = $high - $low + 1;
-		if (($high+$low)%2){
-			return $count/2;
-        }
-		else{
-			return ($low%2) ? ($count+1)/2 : ($count-1)/2;
-        }
+	$count = $high - $low;
+	if (($high+$low)%2){
+	    return ($count+1)/2;
+	}
+	else{
+	    return ($low%2) ? ($count+2)/2 : ($count+1)/2;
+	}
 }
 
 echo countOdds(1, 10);
